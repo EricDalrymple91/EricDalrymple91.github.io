c8f813d183491e92bd4b0c36591361d6eeea98f2
Eric Dalrymple <eric.dalrymple@sureapp.com>
2025-02-05 09:57:38 -0800

    Added all champions pool

--- js/aram.js
+++ js/aram.js
@@ -54,62 +54,55 @@ function championPicker(championPool) {
 }
 
 
-const EricChampions = [
-    "Aatrox",
-    "Gragas",
-    "Kennen",
-    "Lee Sin",
-    "Lulu",
-    "Rumble",
-    "Sylas",
-]
+const leagueOfLegendsChampions = [
+    "Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe",
+    "Aurelion Sol", "Azir", "Bard", "Bel'Veth", "Blitzcrank", "Brand", "Braum", "Briar", "Caitlyn", "Camille",
+    "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise",
+    "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar",
+    "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna",
+    "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin",
+    "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "KSante", "LeBlanc",
+    "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai",
+    "Milio", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Naafiri", "Nami", "Nasus", "Nautilus",
+    "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu & Willump", "Olaf", "Orianna", "Ornn", "Pantheon",
+    "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renata Glasc", "Renekton",
+    "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco",
+    "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra",
+    "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere",
+    "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi",
+    "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao",
+    "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra",
+    "Hwei", "Smolder", "Aurora", "Ambessa", "Mel",
+];
 
-const MickeyChampions = [
-    "Aatrox",
-    "Gragas",
-    "Kennen",
-    "Lee Sin",
-    "Lulu",
-    "Rumble",
-    "Sylas",
-]
 
-const CaitlinChampions = [
-    "Aatrox",
-    "Gragas",
-    "Kennen",
-    "Lee Sin",
-    "Lulu",
-    "Rumble",
-    "Sylas",
-]
+const EricChampions = leagueOfLegendsChampions.filter(
+    item => ![
+        "Hwei",
+    ].includes(item)
+);
 
-const BenChampions = [
-    "Aatrox",
-    "Gragas",
-    "Kennen",
-    "Lee Sin",
-    "Lulu",
-    "Rumble",
-    "Sylas",
-]
+const MickeyChampions = leagueOfLegendsChampions.filter(
+    item => ![
+    ].includes(item)
+)
 
-const PatChampions = [
-    "Aatrox",
-    "Gragas",
-    "Kennen",
-    "Lee Sin",
-    "Lulu",
-    "Rumble",
-    "Sylas",
-]
+const CaitlinChampions = leagueOfLegendsChampions.filter(
+    item => ![
+    ].includes(item)
+)
 
-const HogiChampions = [
-    "Aatrox",
-    "Gragas",
-    "Kennen",
-    "Lee Sin",
-    "Lulu",
-    "Rumble",
-    "Sylas",
-]
+const BenChampions = leagueOfLegendsChampions.filter(
+    item => ![
+    ].includes(item)
+)
+
+const PatChampions = leagueOfLegendsChampions.filter(
+    item => ![
+    ].includes(item)
+)
+
+const HogiChampions = leagueOfLegendsChampions.filter(
+    item => ![
+    ].includes(item)
+)
